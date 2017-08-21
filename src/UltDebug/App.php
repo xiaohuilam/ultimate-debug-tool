35c6170bdd12b4f3a26e6fd68300aeea0584cfd9
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2017-08-20 22:05:38 -0500

    Update App.php

--- src/UltDebug/App.php
+++ src/UltDebug/App.php
@@ -5,6 +5,7 @@ class App{
     protected $msg_if_fail;
     protected $goups = [];
     protected $hide_entities = [];
+    protected $reset_condition = 'false';
 
     public function appendGroup(string $group = 'GROUP NAME', $data){
         $this->groups[$group] = $data;
@@ -12,6 +13,10 @@ class App{
 
     public function hideEntities(array $list = []){
         foreach($list as $k=>$v) $this->hide_entities[$v] = 1;
+    }
+
+    public function resetIf(string $condition_if = 'json.code == 2015'){
+        $this->reset_condition = $condition_if;
     }
 
     public function successIf(string $condition_if_success = 'json.code == 10000'){
@@ -208,6 +213,9 @@ HTML;
         $output .= '$("#log").scrollTop(50000); if('.$this->condition."){
             $('#log').text($('#log').text()+xhr.url+' OK'+' '+'\\r\\n');
         }else{
+            if('.$this->reset_condition.'){
+                window.i=0;
+            }
             $('#log').text($('#log').text()+xhr.url+' FAIL '+".$this->msg_if_fail."+' '+'\\r\\n'); return;
         }";
 
